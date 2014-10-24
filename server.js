2774acb223b89ac974bb2313ac48e84269df5690
Romain Dauby <romain.dauby@gmail.com>
2014-10-24 15:48:49 +0200

    Refacto server.js + nickname

--- server.js
+++ server.js
@@ -1,44 +1,71 @@
 var io = require('socket.io').listen(80);
 
-var players = [];
-var haveToPlay = 0;
+var players = {};
 
-function nextPlayer(number) {
-    return number == 0 ? 1 : 0;
+function pairOfPlayers(id) {
+    if (!players[id].associatedId) {
+        return 1;
+    } else {
+        return 2;
+    }
 }
 
 io.sockets.on('connection', function (socket) {
-    if (players.length <= 2) {
-        players.push(socket.id);
-        if (socket.id == players[haveToPlay]) {
-            socket.emit('canPlay', 'true');
-            socket.broadcast.emit('canPlay', 'false');
-        } else {
+    var nick = socket.id.substr(0, 4);
+    players[socket.id] = {
+        canPlay: false,
+        nick: nick
+    };
+
+    socket.emit('nickname', nick);
+
+    players[socket.id].canPlay = false;
+    socket.emit('canPlay', 'false');
+
+    for (var id in players) {
+        if (players.hasOwnProperty(id) && !players[id].associatedId && id != socket.id) {
+            players[id].associatedId = socket.id;
+            players[socket.id].associatedId = id;
+
+            players[id].canPlay = true;
+            socket.broadcast.to(id).emit('canPlay', 'true');
             socket.emit('canPlay', 'false');
-            socket.broadcast.emit('canPlay', 'true');
+
+            socket.emit('oNick', players[id].nick);
+            socket.broadcast.to(id).emit('oNick', nick);
+
+            break;
         }
-        io.emit('players', players.length);
-        console.log('user connected');
-
-        socket.on('velocity', function (msg) {
-            if (this.id == players[haveToPlay]) {
-                socket.broadcast.emit('canPlay', 'true');
-                console.log(msg);
-                this.broadcast.emit('velocity', msg);
-                haveToPlay = nextPlayer(haveToPlay);
-            }
-        });
-
-        socket.on('disconnect', function () {
-            for (var i = 0; i < players.length; i++) {
-                if (players[i] == socket.id) {
-                    players.splice(i, 1);
-                    haveToPlay = 0;
-                    break;
-                }
-            }
-            this.broadcast.emit('players', players.length);
-            console.log('user disconnected');
-        });
     }
+
+    socket.emit('players', pairOfPlayers(socket.id));
+    socket.broadcast.to(players[socket.id].associatedId).emit('players', pairOfPlayers(socket.id));
+
+    console.log(socket.id + ' : connected');
+
+    socket.on('velocity', function (msg) {
+        if (players[this.id].canPlay && players[this.id].associatedId) {
+            this.broadcast.to(players[this.id].associatedId).emit('velocity', msg);
+            this.broadcast.to(players[this.id].associatedId).emit('canPlay', 'true');
+
+            players[this.id].canPlay = false;
+            if (players[players[this.id].associatedId]) {
+                players[players[this.id].associatedId].canPlay = true;
+            }
+
+            console.log(this.id + ' : ' + msg);
+        }
+    });
+
+    socket.on('disconnect', function () {
+        if (players[this.id].associatedId) {
+            players[players[this.id].associatedId].canPlay = true;
+            delete players[players[this.id].associatedId].associatedId;
+            this.broadcast.to(players[this.id].associatedId).emit('canPlay', 'true');
+            this.broadcast.to(players[this.id].associatedId).emit('players', pairOfPlayers(this.id));
+        }
+        delete players[this.id];
+
+        console.log(this.id + ' : disconnected');
+    });
 });
