925e2d4d8ac7f049678ae4dd1197ee6418ea0141
Romain Dauby <romain.dauby@gmail.com>
2014-10-24 19:02:35 +0200

    New chat with nicknames

--- chat.js
+++ chat.js
@@ -1,14 +1,53 @@
 var io = require('socket.io').listen(8000);
 
+var usernames = {};
+var numUsers = 0;
+
 io.sockets.on('connection', function (socket) {
-    var nick = socket.id.substr(0, 4);
-    console.log(nick + ' connected on chat');
-    socket.on('chat message', function (msg) {
-        var message = nick + ' : ' + msg;
-        console.log(message);
-        io.emit('chat message', message);
+    var addedUser = false;
+
+    socket.on('new message', function (data) {
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
     });
+
+    socket.on('add user', function (username) {
+        socket.username = username;
+        usernames[username] = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
     socket.on('disconnect', function () {
-        console.log(nick + ' disconnected');
+        if (addedUser) {
+            delete usernames[socket.username];
+            --numUsers;
+
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
     });
 });
